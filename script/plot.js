fb800395472be8e9bcc32035b383b1b35350acc5
pugzz <pugzzf@proton.me>
2025-11-25 13:57:22 -0700

    Added subplots to test plot

--- script/plot.js
+++ script/plot.js
@@ -1,13 +1,43 @@
 let exp = "Math.sin(x)"
 
-const yValues = [];
-const xValues = [];
-for (let x = 0; x < 10; x += 0.1)
+var trace1 =
 {
-    xValues.push(x);
-    yValues.push(eval(exp));
-}
+    x: [1, 2, 3],
+    y: [4, 5, 6],
+    type: "scatter"
+};
 
-const data = [{x: xValues, y: yValues, mode:"markers"}];
-const layout = {title: "y = " + exp};
+var trace2 =
+{
+    x: [1, 2, 3],
+    y: [32, 40, 43],
+    xaxis: 'x2',
+    yaxis: 'y2',
+    type: "scatter"
+};
+
+var trace3 =
+{
+    x: [1, 2, 3],
+    y: [210, 250, 230],
+    xaxis: 'x3',
+    yaxis: 'y3',
+    type: "scatter"
+};
+
+var trace4 =
+{
+    x: [1, 2, 3],
+    y: [10, 8, 9],
+    xaxis: 'x4',
+    yaxis: 'y4',
+    type: "scatter"
+};
+
+var data = [trace1, trace2, trace3, trace4];
+
+var layout =
+{
+    grid: {rows: 2, columns: 2, pattern: 'independent'},
+}
 Plotly.newPlot("test", data, layout)
